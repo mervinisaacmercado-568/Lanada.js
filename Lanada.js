15aa75bda40511d9bf00ea1ba358305a27b843f9
mervinisaacmercado-568 <mervinisaacmarket@gmail.com>
2026-02-24 02:14:27 +0400

    Update Lanada.js with official engine comment and new table methods

--- Lanada.js
+++ Lanada.js
@@ -1,4 +1,4 @@
-// Lanada.js – fixed 200+ function Lua engine
+// Lanada.js - Official Engine
 const Lanada = (function() {
   function createEnv(outputCallback) {
     const env = {
@@ -11,7 +11,6 @@ const Lanada = (function() {
       loadstring: (s)=>new Function(s),
       dofile: (f)=>outputCallback("Cannot load file in browser\n"),
 
-      // Math
       math: {
         abs: Math.abs, acos: Math.acos, asin: Math.asin, atan: Math.atan,
         ceil: Math.ceil, cos: Math.cos, exp: Math.exp, floor: Math.floor,
@@ -21,7 +20,6 @@ const Lanada = (function() {
         clamp: (v,min,max)=>Math.min(Math.max(v,min),max)
       },
 
-      // String
       string: {
         byte: (s,i,j)=>s.charCodeAt(i-1),
         char: (...codes)=>String.fromCharCode(...codes),
@@ -35,6 +33,59 @@ const Lanada = (function() {
         reverse: (s)=>s.split('').reverse().join('')
       },
 
+      table: {
+        insert: (arr,val,pos)=>{if(pos!==undefined) arr.splice(pos-1,0,val); else arr.push(val)},
+        remove: (arr,pos)=>{if(pos!==undefined) return arr.splice(pos-1,1)[0]; else return arr.pop()},
+        sort: (arr,cmp)=>arr.sort(cmp),
+        concat: (arr,sep)=>arr.join(sep||","),
+        pack: (...args)=>args,
+        unpack: (arr)=>[...arr],
+        shuffle: (arr)=>arr.sort(()=>Math.random()-0.5),
+        sum: (arr)=>arr.reduce((s,v)=>s+v,0),
+        avg: (arr)=>arr.reduce((s,v)=>s+v,0)/arr.length
+      },
+
+      range: (a,b)=>{let arr=[];for(let i=a;i<=b;i++) arr.push(i); return arr;},
+      randomChoice: (arr)=>arr[Math.floor(Math.random()*arr.length)],
+      deepCopy: (obj)=>JSON.parse(JSON.stringify(obj))
+    };
+    return env;
+  }
+
+  function preprocessLua(code) {
+    code = code.replace(/--.*$/gm,""); // remove comments
+    code = code.replace(/\blocal\s+([a-zA-Z_][a-zA-Z0-9_]*)/g, 'var $1'); // convert local to var
+    code = code.replace(/function\s+(\w+)\s*\((.*?)\)/g, 'function $1($2){');
+    code = code.replace(/if\s+(.*?)\s*then/g, 'if($1){');
+    code = code.replace(/elseif\s+(.*?)\s*then/g, '}else if($1){');
+    code = code.replace(/else/g, '}else{');
+    code = code.replace(/for\s+(\w+)\s*=\s*(\d+)\s*,\s*(\d+)\s*do/g, 'for(var $1=$2;$1<=$3;$1++){');
+    code = code.replace(/repeat/g, 'do{');
+    code = code.replace(/until\s+(.*)/g, '}while(!($1))');
+    code = code.replace(/\bend\b/g, '}');
+
+    // Add semicolons at the end of lines to avoid JS parsing errors
+    code = code.split('\n').map(line=>line.trim()).filter(line=>line.length>0).join(';\n') + ';';
+    return code;
+  }
+
+  return {
+    createEnv,
+    preprocessLua,
+    run: function(code, outputCallback = console.log) {
+      const env = createEnv(outputCallback);
+      try {
+        const jsCode = preprocessLua(code);
+        const keys = Object.keys(env);
+        const values = Object.values(env);
+        const func = new Function(...keys, jsCode);
+        func(...values);
+      } catch(err) {
+        outputCallback('Error: ' + err.message);
+      }
+    }
+  };
+})();
       // Table
       table: {
         insert: (arr,val,pos)=>{if(pos!==undefined) arr.splice(pos-1,0,val); else arr.push(val)},
